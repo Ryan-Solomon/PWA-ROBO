39729ce72a4f8a5790e12723e7ff46c0cf3fb3ad
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-07-17 15:50:18 -0500

    Updates

---
+++ precache-manifest.51040f942f46cd279b7efe9824ac42a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6d6f2692884c4627c2eed6331a91f2",
+    "url": "/PWA-ROBO/index.html"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/PWA-ROBO/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a0cc34e6d6b9cacc626",
+    "url": "/PWA-ROBO/static/css/main.eb5baaa0.chunk.css"
+  },
+  {
+    "revision": "ca5411f3cb874653a63d",
+    "url": "/PWA-ROBO/static/js/2.8bf57d0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PWA-ROBO/static/js/2.8bf57d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0cc34e6d6b9cacc626",
+    "url": "/PWA-ROBO/static/js/main.a2e6bddf.chunk.js"
+  },
+  {
+    "revision": "6cea2aa24a4ec6125ded",
+    "url": "/PWA-ROBO/static/js/runtime-main.d3a95e3f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA-ROBO/static/media/SEGA.12a3db8b.woff"
+  }
+]);
